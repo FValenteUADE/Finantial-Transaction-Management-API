107154f0ca9371cfb34613d009edc1cee559922f
Franco G. Valente <francogabriel.valente.contractor@gdevtools.com>
2025-01-28 15:03:42 -0300

    refactor: Refactor Code Structure
    Refactored the code structure to improve interaction with abstractions within the application, ensuring adherence to SOLID principles and preventing direct interaction with the implementation.
    
    Implements: fvalente@uade.edu.ar

--- src/main/java/com/redlink/psi/service/CurrencyConversionService.java
+++ src/main/java/com/redlink/psi/service/CurrencyConversionService.java
@@ -1,26 +1,7 @@
 package com.redlink.psi.service;
 
-import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 
-@Service
-public class CurrencyConversionService {
-
-    public BigDecimal convertToARS(BigDecimal amount, String currency) {
-        // Para simplificar, asumimos una tasa de cambio ficticia con valores del 27/01/25.
-        BigDecimal conversionRate = getConversionRate(currency, "ARS");
-        return amount.multiply(conversionRate);
-    }
-
-    private BigDecimal getConversionRate(String fromCurrency, String toCurrency) {
-        // Tomamos los valores de la moneda BLUE en cada caso.
-        if (fromCurrency.equals("USD") && toCurrency.equals("ARS")) {
-            return BigDecimal.valueOf(1230);
-        }
-        if (fromCurrency.equals("EUR") && toCurrency.equals("ARS")) {
-            return BigDecimal.valueOf(1328);
-        }
-        return BigDecimal.ONE;
-    }
+public interface CurrencyConversionService {
+    BigDecimal convertToARS(BigDecimal amount, String currency);
 }
---
+++ src/main/java/com/redlink/psi/service/implementation/CurrencyConversionServiceImpl.java
@@ -0,0 +1,27 @@
+package com.redlink.psi.service.implementation;
+
+import com.redlink.psi.service.CurrencyConversionService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CurrencyConversionServiceImpl implements CurrencyConversionService {
+
+    public BigDecimal convertToARS(BigDecimal amount, String currency) {
+        // Para simplificar, asumimos una tasa de cambio ficticia con valores del 27/01/25.
+        BigDecimal conversionRate = getConversionRate(currency, "ARS");
+        return amount.multiply(conversionRate);
+    }
+
+    private BigDecimal getConversionRate(String fromCurrency, String toCurrency) {
+        // Tomamos los valores de la moneda BLUE en cada caso.
+        if (fromCurrency.equals("USD") && toCurrency.equals("ARS")) {
+            return BigDecimal.valueOf(1230);
+        }
+        if (fromCurrency.equals("EUR") && toCurrency.equals("ARS")) {
+            return BigDecimal.valueOf(1328);
+        }
+        return BigDecimal.ONE;
+    }
+}
